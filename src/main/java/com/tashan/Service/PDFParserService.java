e6f1b376d62c2062ea4d2e4bbdb76c5f530df00d
ONUR TASHAN <onur.tashan@turkcell.com.tr>
2017-06-20 10:33:36 +0300

    Service Class fixed.

--- src/main/java/com/tashan/Service/PDFParserService.java
+++ src/main/java/com/tashan/Service/PDFParserService.java
@@ -1,15 +1,9 @@
 package com.tashan.Service;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
-
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * Created by TCOTASHAN on 19.6.2017.
@@ -19,24 +13,24 @@ public class PDFParserService {
     public String[] parserSonuc(int isParsed) throws IOException {
 
             URL url = new URL("http://www.millipiyango.gov.tr/node/themes/bluemasters/documents/Bahisler/Kampanya%20%C4%B0zinleri.pdf");
-            //PDDocument doc = PDDocument.load(url.openStream());
+            PDDocument doc = PDDocument.load(url.openStream());
             PDFTextStripper stripper = new PDFTextStripper();
 
-            PDFParser parser = new PDFParser(url.openStream());
-
-            parser.parse();
-            COSDocument cosDoc = parser.getDocument();
-            PDDocument doc = new PDDocument(cosDoc);
-            stripper.setStartPage(1);
-            stripper.setEndPage(1);
-            String sonuc = stripper.getText(doc);
-            String[] sonuclar = sonuc.split("Ticari");
-
-            System.out.println(sonuclar.length);
-
+            if(isParsed == 1){
+                    stripper.setStartPage(1);
+                    stripper.setEndPage(1);
+                    String sonuc = stripper.getText(doc);
+                    String[] parsed = sonuc.split("Ticari");
+                    System.out.println("PDF başarılı bir şekilde okundu.");
+                    doc.close();
+                    return parsed;
+            }else{
+                    System.out.println("PDF başarılı bir şekilde okundu ama parse edilmedi.");
+            }
             doc.close();
-            System.out.println("PDF başarılı bir şekilde okundu.");
 
-            return sonuclar;
+            String[] fails = {"Parse edilmek isteniyor ise 1 değeri ile webservis çağırılmalıdır."};
+
+            return fails;
     }
 }
